befdbb06578f8666487db4b2905c50e4a2906dc2
DanielMorales00 <89607489+DanielMorales00@users.noreply.github.com>
2021-11-08 23:44:49 -0300

    Add files via upload

--- Clases TXT actializadas v2/SistemaTienda.java
+++ Clases TXT actializadas v2/SistemaTienda.java
@@ -1,16 +1,16 @@
-
 package taller1;
+
 
 public interface SistemaTienda {
     public boolean AgregarPersonaje(String nombrePersonaje,String rol); //listo
     public boolean AgregarAspecto(String nombreAspecto, String calidad); // listo
     public void AsociarPersonajeAspecto(String nombrePersonaje, String nombreAspecto);
-    public boolean AgregarCuenta(String nombreCuenta, String contraseÃ±a, String nombreUsuario, int nivel,int rpEnCuenta, String Servidor);
+    public boolean AgregarCuenta(String nombreCuenta, String contraseña, String nombreUsuario, int nivel,int rpEnCuenta, String Servidor);
     public void AsociarPersonajePoseidoCuenta(String nombrePersonaje ,String nombreCuenta);
     // "Contrato Sospechoso"  public void AsociarAspectoCuenta(String nombreAspectoPoseido,String nombreCuenta);
-    public boolean IniciarSesion(String nombreCuenta, String contraseÃ±a);
+    public boolean IniciarSesion(String nombreCuenta, String contraseña);
     public Cuenta EstablecerCUenta(String nombreCuenta);
-    public boolean RegistrarCuenta(String nombreCuenta,String contraseÃ±a, String nombreUsuario,String Servidor);
+    public boolean RegistrarCuenta(String nombreCuenta,String contraseña, String nombreUsuario,String Servidor);
     public int RecaudacionPersonaje(String nombrePersonaje);
     public AspectoPoseido ComprarAspecto(String Cuenta, String nombrePersonajePoseido, String nombreAspectoPoseido);
     public PersonajePoseido ComprarPersonaje(String Cuenta, String nombrePersonajePoseido);
@@ -21,7 +21,7 @@ public interface SistemaTienda {
     ///////////////////////////////// trabajo pendiente: Documentar con javaDoc todos los contratos de esta interfaz /////////////////////////////////
    /** public String mostrarPersonaje(PersonajePoseido personaje);
     public String mostrarAspecto(AspectoPoseido aspecto);
-    public void CambiarContraseÃ±a(Cuenta cuenta, String contraseÃ±a);
+    public void CambiarContraseña(Cuenta cuenta, String contraseña);
     public void MostrarRecaudacionRol(ListaPersonaje listaPersonaje);
     public void MostrarRecaudacionRegion(ListaCuentas listacuenta);
     public void MostrarPersonajeRol(ListaPersonaje listaPersonaje);
